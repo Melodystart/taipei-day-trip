bef61b967cc1dc7ca00ab972ab770ec6bda56ced
Melodystart <start99start@gmail.com>
2023-08-29 09:46:35 +0800

    feat: add 3 API

---
+++ data/data.py
@@ -0,0 +1,36 @@
+import json
+import mysql.connector
+
+with open('taipei-attractions.json',encoding="utf-8") as f:
+    original_data = json.load(f)
+
+data = original_data["result"]["results"]
+
+con = mysql.connector.connect(
+    host='localhost',           
+    user='root',                
+    password='password',
+)
+cursor = con.cursor()
+
+# 建立資料庫、table
+cursor.execute("CREATE database attractions;")
+cursor.execute("USE attractions;")
+cursor.execute("CREATE table main (id BIGINT PRIMARY KEY NOT NULL,name VARCHAR(255),category VARCHAR(255),description TEXT,address TEXT,transport TEXT,mrt VARCHAR(255),lat DECIMAL(8,6),lng DECIMAL(9,6));")
+cursor.execute("CREATE table image (id BIGINT PRIMARY KEY auto_increment,attraction_id BIGINT NOT NULL,images TEXT);")
+
+# 將資料存入MySQL
+for i in range(len(data)):
+# 圖片以外的資料存放table main
+  cursor.execute("INSERT INTO main (id, name, category, description, address, transport, mrt, lat, lng) VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s)",(data[i]["_id"], data[i]["name"],data[i]["CAT"],data[i]["description"],data[i]["address"],data[i]["direction"],data[i]["MRT"],data[i]["latitude"],data[i]["longitude"]))
+  con.commit()
+# 圖片資料存放table image
+  arr = data[i]["file"].lower().split('https://')
+  for item in arr:
+    if 'jpg' in item.lower() or 'png' in item.lower():
+      item = 'https://' + item
+      cursor.execute("INSERT INTO image (attraction_id, images) VALUES (%s, %s)",(data[i]["_id"], item))
+      con.commit()
+
+cursor.close()
+con.close()
